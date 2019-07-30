e1146bb36d39beeffcbca5862ab3c4cec7fbbe7b
Kamga Mfunyom <hans.kamgamfunyom@cgi.com>
2019-07-30 15:20:22 -0400

    Junit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
+package junit;
+
 public class Hello{
 
      public static void main(String []args){
         System.out.println("Hello World everybody");
      }
+	 
+	 public boolean isGreater(int num1, int num2){
+        return num1 > num2;
+    }
 }
